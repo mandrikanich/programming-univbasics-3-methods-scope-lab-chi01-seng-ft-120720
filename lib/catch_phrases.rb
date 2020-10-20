d2e69e557d3b9ffb29302656796f9e52c073dbf3
Michael Andrikanich <mandrikanich@gmail.com>
2020-10-20 20:23:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def link
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'It's Dangerous To Go Alone! Take This.'
   phrase = 'It's-a me, Mario!''
   puts phrase
 end
